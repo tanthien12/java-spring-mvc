787a5688dfeeee92dac2b3d3dde341b66f6176ec
tanthien12 <nguyentanthien20.12@gmail.com>
2024-11-14 15:06:13 +0700

    Update code

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,7 +8,17 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World With Nguyen Tan Thien!";
+        return "Hello World With Nguyen Tan Thien!!!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page!";
     }
 
 }
